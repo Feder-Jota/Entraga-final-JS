588a1834594eedf520bfbea56bfbb65b0c51081a
Federico Juarez <112991094+Feder-Jota@users.noreply.github.com>
2023-02-12 15:00:50 -0300

    Commit5

--- script.js
+++ script.js
@@ -3,48 +3,47 @@ let menuBurgas = [
     id: 0,
     nombre: "La 1",
     precio: 1000,
-    img: "Imagenes/b1.jpg"
+    img: "imagenes/b1.jpg"
   },
   {
     id: 1,
     nombre: "La 2",
     precio: 1200,
-    img: "Imagenes/b1.jpg"
+    img: "imagenes/b1.jpg"
   },
   {
     id: 2,
     nombre: "La 3",
     precio: 1400,
-    img: "Imagenes/b1.jpg"
+    img: "imagenes/b1.jpg"
   },
   {
     id: 3,
     nombre: "La 4",
     precio: 1000,
-    img: "Imagenes/b1.jpg"
+    img: "imagenes/b1.jpg"
   },
   {
     id: 4,
     nombre: "La 5",
     precio: 1000,
-    img: "Imagenes/b1.jpg"
+    img: "imagenes/b1.jpg"
   },
   {
     id: 5,
     nombre: "La 6",
     precio: 1200,
-    img: "Imagenes/b1.jpg"
+    img: "imagenes/b1.jpg"
   },
   {
     id: 6,
     nombre: "La 7",
     precio: 1400,
-    img: "Imagenes/b1.jpg"
+    img: "imagenes/b1.jpg"
   }
 ]
 
 function miPagina(menu) {
-  function miPagina(menu) {
   let body = document.getElementById("body")
   let pillsTab = document.getElementById("pills-tab")
   let pillsProfileTab = document.getElementById("pills-profile-tab")
@@ -74,6 +73,14 @@ function show() {
 }
 show()
 
+let carrito = []
+let botones = document.getElementsByClassName("boton")
+for (const boton of botones) {
+  boton.addEventListener("click", agregarAlCarrito)
+}
+
+function agregarAlCarrito(e) {
+
   let ordenPedido = menuBurgas.find(pedido => pedido.id == e.target.id)
   let posicionBuscada = carrito.findIndex(pedido => pedido.id == ordenPedido.id)
   if (posicionBuscada != -1) {
@@ -82,5 +89,5 @@ show()
   } else {
     carrito.push({ id: ordenPedido.id, nombre: ordenPedido.nombre, precio: ordenPedido.precio, unidades: 1, precioTotal: ordenPedido.precio })
   }
+  console.log(carrito)
 }
-console.log(carrito)
